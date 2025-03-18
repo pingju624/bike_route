f45138e32e5c7f96e8c2d3234e1284e234bca7dd
pingju624 <153528111+pingju624@users.noreply.github.com>
2025-03-18 09:42:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ if uploaded_file:
         folium.Marker(
             location=[row["lat"], row["lon"]],
             icon=folium.DivIcon(html=f"""
-                <div style="font-size: 8px; font-weight: bold; color: black; text-align: center;style="width: 100px;">
+                <div style="font-size: 8px; font-weight: bold; color: black; text-align: center; width: 100px;">
                     {row['name']}
                 </div>
             """),
